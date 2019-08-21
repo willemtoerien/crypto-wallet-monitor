9be2212209622fe3d783e4352d99d1ed969b73b1
Willem Toerien <wtoerien@wtprograms.com>
2019-08-21 16:30:34 +0200

    Added the initial wallet source files.

---
+++ Wallet/Wallet.Database/dbo/Tables/Transactions.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Transactions] (
+    [TransactionId] UNIQUEIDENTIFIER NOT NULL,
+    [FromUserId]    INT              NOT NULL,
+    [ToUserId]      INT              NOT NULL,
+    [Amount]        DECIMAL (18, 2)  NOT NULL,
+    [Purpose]       NVARCHAR (300)   NULL,
+    [TransactionAt] DATETIME         NOT NULL,
+    CONSTRAINT [PK_Transactions] PRIMARY KEY CLUSTERED ([TransactionId] ASC),
+    CONSTRAINT [FK_Transactions_Users_From] FOREIGN KEY ([FromUserId]) REFERENCES [dbo].[Users] ([UserId]),
+    CONSTRAINT [FK_Transactions_Users_To] FOREIGN KEY ([ToUserId]) REFERENCES [dbo].[Users] ([UserId])
+);
+
---
+++ Wallet/Wallet.Database/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [UserId]   INT             IDENTITY (1, 1) NOT NULL,
+    [Email]    NVARCHAR (300)  NOT NULL,
+    [Password] NVARCHAR (100)  NOT NULL,
+    [Balance]  DECIMAL (18, 2) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
